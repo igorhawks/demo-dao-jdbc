6316f5f6286f9e7eac49e2a55b362816bd900499
Igor Silva <igor719@gmail.com>
2024-02-13 15:16:53 -0300

    FindById

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -2,20 +2,13 @@ package model.dao;
 
 import java.util.List;
 
-
 import model.entities.Seller;
 
 public interface SellerDao {
-	
-	void insert(Seller obj);
-	
-	void update(Seller obj);
-	
-	void deleteById( Integer id);
-	
-	Seller findById(Integer id);
-	
-	List<Seller> findAll();
-	
 
+	void insert(Seller obj);
+	void update(Seller obj);
+	void deleteById(Integer id);
+	Seller findById(Integer id);
+	List<Seller> findAll();
 }
